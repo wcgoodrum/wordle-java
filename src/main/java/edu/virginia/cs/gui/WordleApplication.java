482748eebf74eabcb8af6ae45020d6afc32008b9
Tammy Ngo <bsy6pq@virginia.edu>
2023-03-30 11:36:12 -0400

    Updated title of Stage and dimensions of Scene in WordleApplication.

--- src/main/java/edu/virginia/cs/gui/WordleApplication.java
+++ src/main/java/edu/virginia/cs/gui/WordleApplication.java
@@ -11,8 +11,8 @@ public class WordleApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(WordleApplication.class.getResource("wordle-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 500, 600);
+        stage.setTitle("Wordle");
         stage.setScene(scene);
         stage.show();
     }
