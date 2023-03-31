d07e4874945e9f2c28c962a11b9b64684aa5104d
Tammy Ngo <bsy6pq@virginia.edu>
2023-03-31 13:42:52 -0400

    Updated dimensions of Scene in WordleApplication and updated formatting of wordle-view.fxml.

--- src/main/java/edu/virginia/cs/gui/WordleApplication.java
+++ src/main/java/edu/virginia/cs/gui/WordleApplication.java
@@ -11,7 +11,7 @@ public class WordleApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(WordleApplication.class.getResource("wordle-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 420, 560);
         stage.setTitle("Wordle");
         stage.setScene(scene);
         stage.show();
