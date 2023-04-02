e47552bfcbc27e9843cf5b145a9e3b77f6efcd35
schhahira <sc9ugn@virginia.edu>
2023-04-02 15:38:48 -0400

    initial creating of yes and no buttons for when the game is over

--- src/main/java/edu/virginia/cs/gui/WordleController.java
+++ src/main/java/edu/virginia/cs/gui/WordleController.java
@@ -1,14 +1,34 @@
 package edu.virginia.cs.gui;
 
+import edu.virginia.cs.wordle.Wordle;
+import edu.virginia.cs.wordle.WordleImplementation;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class WordleController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button newGameButton;
+    @FXML
+    private Button endGameButton;
+
+    @FXML
+    protected void buttons_initialize() {
+        newGameButton.setVisible(false);
+        endGameButton.setVisible(false);
+        // set to true if game is over
     }
+
+    @FXML
+    protected void restartGame() {
+        // reset variables
+    }
+
+    @FXML
+    protected void endGame(ActionEvent event){
+        javafx.application.Platform.exit();
+    }
+
 }
